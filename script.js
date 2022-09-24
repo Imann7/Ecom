a54073f50002b3e08c89b76e0c216506bc998f17
Imann <113292209+Imann7@users.noreply.github.com>
2022-09-24 16:45:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const toggle = document.getElementById("toggleDark");
+const menu = document.getElementById("toggleMenu");
 const body = document.querySelector("body");
+const list = document.querySelector("ul");
 
 toggle.addEventListener("click", function () {
   this.classList.toggle("fa-moon");
   document.body.classList.toggle("dark-theme");
   document.body.style.transition("1s");
+});
+
+menu.addEventListener("click", () => {
+  list.classList.toggle("active");
 });
